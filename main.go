ac51860df51453587228e5091c4031922499d170
John <gtom_1984@hotmail.ca>
2025-05-01 13:59:32 -0700

    Fix up formatting

--- main.go
+++ main.go
@@ -27,11 +27,13 @@ var staticFiles embed.FS
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)}
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT environment variable is not set")}
+		log.Fatal("PORT environment variable is not set")
+	}
 
 	apiCfg := apiConfig{}
 
